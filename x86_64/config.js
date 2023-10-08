7859981851eb5bf1edef8716a84e6bb7c5d364fa
Megah <75638685+endblack@users.noreply.github.com>
2023-10-08 18:01:53 -0300

    Update config.js

--- x86_64/config.js
+++ x86_64/config.js
@@ -2,9 +2,9 @@ config = {
   dono:"559184035474", //seu número com 55+ddd sem o 9
   tempo_teste:"3", //tempo do teste em horas
   valorLogin: 0.10, //valor do login SSH
-  nomeLoja:"Megah Store", //nome da sua loja virtual
-  linkApp:"https://play.google.com/store/apps/details?id=com.hunter.net", //Link do seu app, playStore/Mediafire
-  token_mp:"APP_USR-7717444308631755-011616-ea50912e55820ae536cc3e59b85d0545-1141576080" //seu token do mercado pago
+  nomeLoja:"Loja teste", //nome da sua loja virtual
+  linkApp:"LinkAqui", //Link do seu app, playStore/Mediafire
+  token_mp:"tokenAqui" //seu token do mercado pago
 }
 module.exports = {
   config
