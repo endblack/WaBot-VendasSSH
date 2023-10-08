ca64c1538190baed221ce67bba5bbb929afc93cf
Megah <75638685+endblack@users.noreply.github.com>
2023-10-07 23:15:37 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+apt update -y
+apt install nodejs -y
+apt install wget -y
+apt install unzip -y
+apt install screen -y
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+source ~/.bashrc
+nvm install 16
+wget https://raw.githubusercontent.com/endblack/WaBot-VendasSSH/main/src/onbot
+wget https://github.com/endblack/WaBot-VendasSSH/raw/main/src/offbot
+wget https://github.com/endblack/WaBot-VendasSSH/raw/main/src/qrcode
+chmod +x onbot offbot qrcode
+mv onbot offbot qrcode /bin
+wget https://github.com/endblack/WaBot-VendasSSH/raw/main/megahbot.zip -O /etc/megahbot.zip
+unzip /etc/megahbot.zip
+echo
+echo "Tudo certo, digite o comando qrcode para fazer login no whatsapp
+echo
