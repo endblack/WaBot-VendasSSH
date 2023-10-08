502000c5f8c3da64be87386ecf1fe19bde275a60
Megah <75638685+endblack@users.noreply.github.com>
2023-10-08 20:44:46 -0300

    Delete i.sh

--- i.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-apt update -y
-apt install wget -y
-wget https://github.com/endblack/WaBot-VendasSSH/raw/main/$(uname -i)/install.sh
-chmod 777 install.sh
-./install.sh
-rm install.sh
