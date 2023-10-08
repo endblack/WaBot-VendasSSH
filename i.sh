a42bbbd7780a7256e111519a21e4b6ddfa34e46f
Megah <75638685+endblack@users.noreply.github.com>
2023-10-08 15:33:40 -0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -2,7 +2,7 @@
 
 apt update -y
 apt install wget -y
-wget https://github.com/endblack/WaBot-VendasSSH/raw/main/install.sh
+wget https://github.com/endblack/WaBot-VendasSSH/raw/main/$(uname -i)/install.sh
 chmod 777 install.sh
 ./install.sh
 rm install.sh
