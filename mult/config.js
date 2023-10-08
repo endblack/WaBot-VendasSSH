f7822b3a2fe012ad0f2c31ad9362b00a7bab86ec
Megah <75638685+endblack@users.noreply.github.com>
2023-10-08 20:43:48 -0300

    Add files via upload

---
+++ mult/config.js
@@ -0,0 +1,11 @@
+config = {
+  dono:"559184035474", //seu número com 55+ddd sem o 9
+  tempo_teste:"3", //tempo do teste em horas
+  valorLogin: 0.10, //valor do login SSH
+  nomeLoja:"Loja teste", //nome da sua loja virtual
+  linkApp:"LinkAqui", //Link do seu app, playStore/Mediafire
+  token_mp:"TokenAqui" //seu token do mercado pago
+}
+module.exports = {
+  config
+}
